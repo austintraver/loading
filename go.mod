12ea16ee515c40518ff2c074a283037a0ac64ad9
Austin Traver <austintraver@gmail.com>
2022-01-24 15:06:37 -0800

    Experiment with module

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/austintraver/loading
 
-go 1.16
+go 1.17
+
+require (
+	github.com/gosuri/uilive v0.0.4
+	github.com/mattn/go-isatty v0.0.14 // indirect
+)
--- root.go
+++ root.go
@@ -4,72 +4,44 @@ package loading
 
 import (
 	"fmt"
-	"sync/atomic"
 	"time"
-)
 
-// states are what characters to cycle through as a Spinner is running
-var states = []rune{'⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', '⠋', '⠙', '⠹'}
+	"github.com/gosuri/uilive"
+)
 
 // Spinner main type
 type Spinner struct {
-	frames []rune
-	index  int
-	active uint64
-	text   string
+	interval time.Duration
+	writer   *uilive.Writer
 }
 
 // New Spinner with args
-func New(text string) *Spinner {
-	s := &Spinner{
-		text: ("\r\033[K") + text,
+func New(interval time.Duration) *Spinner {
+	return &Spinner{
+		interval: interval,
+		writer:   uilive.New(),
 	}
-	s.Set(states)
-	return s
 }
 
-// Set frames to the given string which must not use spaces.
-func (target *Spinner) Set(frames []rune) {
-	target.frames = frames
-}
-
-// Start shows the spinner.
-func (target *Spinner) Start() *Spinner {
-	if atomic.LoadUint64(&target.active) > 0 {
-		return target
-	}
-	atomic.StoreUint64(&target.active, 1)
-	go func() {
-		for atomic.LoadUint64(&target.active) > 0 {
-			fmt.Printf(target.text, target.next())
-			time.Sleep(100 * time.Millisecond)
+func (s *Spinner) Loop(duration time.Duration) {
+	deadline := time.Now().Add(duration)
+	// states are what characters to cycle through as a Spinner is running
+	states := []rune{'⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', '⠋', '⠙', '⠹'}
+	for _, c := range states {
+		if time.Now().After(deadline) {
+			break
 		}
-	}()
-	return target
-}
-
-// The clear function moves the cursor to the beginning of the current line
-// and erases any existing text, in order to replace it with the next
-// incremental stage of the Spinner
-func clear() (n int, err error) {
-	return fmt.Print("\r\033[K")
+		_, err := fmt.Fprintf(s.writer, "Loading... %c\n", c)
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(s.interval)
+	}
 }
 
 // The Stop function hides a spinner. The value returned by Stop is true
 // if a running spinner was stopped, and false otherwise.
-func (target *Spinner) Stop() (ok bool, err error) {
-	x := atomic.SwapUint64(&target.active, 0)
-	if x > 0 {
-		_, err = clear()
-		ok = true
-	}
-	return
-}
-
-// The next function advances the current char displayed by the
-// spinner to the symbol directly after the current symbol.
-func (target *Spinner) next() (char string) {
-	char = string(target.frames[target.index%len(target.frames)])
-	target.index++
+func (s *Spinner) Stop() {
+	s.writer.Stop()
 	return
 }
